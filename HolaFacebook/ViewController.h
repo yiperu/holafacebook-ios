09b08a269620fe94433f8cb4f00719cde29a17d2
Henry AT <yiperu@gmail.com>
2013-09-10 03:13:56 -0500

    Commit Inicial

---
+++ HolaFacebook/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HolaFacebook
+//
+//  Created by Henry AT on 9/10/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HolaFacebook/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HolaFacebook
+//
+//  Created by Henry AT on 9/10/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
